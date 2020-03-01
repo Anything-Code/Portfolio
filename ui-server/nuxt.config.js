331974ea419878f24565c2fd5a984b0f5b962aab
Niklas Lübcke <salkin1@hotmail.de>
2020-03-01 15:56:42 +0100

    added nuxt/pwa

--- ui-server/nuxt.config.js
+++ ui-server/nuxt.config.js
@@ -5,7 +5,10 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' }
     ],
   },
-  modules: ['nuxt-material-design-icons'],
+  modules: [
+    '@nuxtjs/pwa',
+    'nuxt-material-design-icons'
+  ],
   css: ['~assets/scss/app.scss'],
   plugins: ['~/plugins/particles.client.js']
 }
