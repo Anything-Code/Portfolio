330794fb91db8d55fcb46cbbc3de9e67c2a8f5bd
Niklas Lübcke <salkin1@hotmail.de>
2020-06-12 13:23:36 +0200

    fixed dom cheks for typewriter plugin

--- ui/plugins/typewriter.client.js
+++ ui/plugins/typewriter.client.js
@@ -9,35 +9,43 @@ export default ({ app }, inject) => {
     }).typeString('Servus, ich bin Niklas Lübcke, 23 Jahre alt und komme aus Heidelberg.') 
       .pauseFor(200)
       .callFunction(() => {
-        document.querySelector('.first-line .Typewriter__cursor').style.display = 'none'
-        document.querySelector('.second-line').style.display = 'block'
-
-        new Typewriter('#second', {
-          cursor: '',
-          delay: 30
-        })
-        .pauseFor(2000)
-        .callFunction(() => {
-          document.querySelector('#second').classList.remove('ml-3')
-        })
-        .typeString('Ich studiere Wirtschaftsinformatik und bin seit 2018 selbständig als Softwareentwickler tätig.')
-        .pauseFor(200)
-        .callFunction(() => {
-          document.querySelector('.second-line .Typewriter__cursor').style.display = 'none'
-          document.querySelector('.third-line').style.display = 'block'
+        if (document.querySelector('#second'))
+        {
+          document.querySelector('.first-line .Typewriter__cursor').style.display = 'none'
+          document.querySelector('.second-line').style.display = 'block'
   
-          new Typewriter('#third', {
+          new Typewriter('#second', {
             cursor: '',
             delay: 30
           })
           .pauseFor(2000)
           .callFunction(() => {
-            document.querySelector('#third').classList.remove('ml-3')
+            if (document.querySelector('#second'))
+            document.querySelector('#second').classList.remove('ml-3')
           })
-          .typeString('Diese Seite dient als Portfolio, auf dem ich über einige meiner bisherigen Projekte und deren Umsetzung informieren möchte.')
+          .typeString('Ich studiere Wirtschaftsinformatik und bin seit 2018 selbständig als Softwareentwickler tätig.')
+          .pauseFor(200)
+          .callFunction(() => {
+            if (document.querySelector('#third'))
+            {
+              document.querySelector('.second-line .Typewriter__cursor').style.display = 'none'
+              document.querySelector('.third-line').style.display = 'block'
+      
+              new Typewriter('#third', {
+                cursor: '',
+                delay: 30
+              })
+              .pauseFor(2000)
+              .callFunction(() => {
+                if (document.querySelector('#third'))
+                document.querySelector('#third').classList.remove('ml-3')
+              })
+              .typeString('Diese Seite dient als Portfolio, auf dem ich über einige meiner bisherigen Projekte und deren Umsetzung informieren möchte.')
+              .start()
+            }
+          })
           .start()
-        })
-        .start()
+        }
       })
       .start()
   })
